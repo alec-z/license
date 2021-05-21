1a3ca2b2e8f43703f30a5730a5a2e745d6499a5c
alec.zheng <zhengzhipeng4@huawei.com>
2021-05-22 06:42:32 +0800

    bug fix

--- license-front/src/app/license-show/license-show.component.ts
+++ license-front/src/app/license-show/license-show.component.ts
@@ -14,6 +14,7 @@ const GET_LICENSE = gql`
             licenseMainTags {
                 mainTag {
                     name
+                    description
                 }
             }
             mustFeatureTags {
